bb2c33b60eb983a2f881d5e98d9a980df2ea6af1
David Ta <taminhtu@gmail.com>
2018-09-09 10:40:10 +0700

    Initial Commit

---
+++ ScrollBookLikeIBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollBookLikeIBook
+//
+//  Created by Ta Minh Tu on 9/9/18.
+//  Copyright © 2018 Ta Minh Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
